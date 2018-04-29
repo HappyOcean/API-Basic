ad4b31bb83f94953bc53d1bcfaaafc1847026338
Hiro <yukihiroiwayama@macbookpro.local>
2018-04-29 13:53:44 +0900

    Initial Commit

---
+++ API-Basic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  API-Basic
+//
+//  Created by Yukihiro Iwayama on 2018/04/29.
+//  Copyright © 2018年 Yukihiro Iwayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
